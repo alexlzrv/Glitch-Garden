e7e970eefa42d4183f3ed0099e7f74831a903b0c
Alex <alexlzrw@gmail.com>
2020-08-31 15:15:01 +0300

    add Load Scene

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    [SerializeField] int timeForWait = 3;
+    int currentIndex;
+
+
+    void Start()
+    {
+        currentIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (currentIndex == 0)
+        {
+            StartCoroutine(WaitForTime());
+        }
+
+    }
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(timeForWait);
+        LoadNExtScene();
+    }
+
+    private void LoadNExtScene()
+    {
+        SceneManager.LoadScene(currentIndex + 1);
+    }
+}

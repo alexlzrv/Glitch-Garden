c8b92e0488cd7fd40321c7bcad855a793842ee43
Alex <alexlzrw@gmail.com>
2020-09-10 16:48:08 +0300

    added projectile damage

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float speed = 1f;
+    [SerializeField] float damage = 50f;
 
     void Update()
     {
         transform.Translate(Vector2.right * Time.deltaTime * speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        var health = otherCollider.GetComponent<Health>();
+
+        health.DealDamage(damage);
     }
 }

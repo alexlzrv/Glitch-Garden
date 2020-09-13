1253bf9c60073dbc07ab57a3d68829bc0cc66a98
Alex <alexlzrw@gmail.com>
2020-09-13 14:40:39 +0300

    defender selection UI

---
+++ Assets/Scripts/DefenderBurtton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderBurtton : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        var buttons = FindObjectsOfType<DefenderBurtton>();
+        foreach(DefenderBurtton button in buttons)
+        {
+            button.GetComponent<SpriteRenderer>().color = new Color32(41, 41, 41, 255);
+        }
+
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+}

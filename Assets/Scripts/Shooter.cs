602d1184ce5cfb5f2386f96f673cbdefbabaed7e
Alex <alexlzrw@gmail.com>
2020-09-21 21:20:05 +0300

    Attacker in my lane

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,13 +1,52 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject projectile, gun;
+    AttackerSpawner myLaneSpawner;
+
+
+    private void Start() {
+        SetLaneSpawner();
+    }
+
+    private void Update() {
+        if (IsAttackerInLane()) {
+            Debug.Log("На линии");
+        }
+        else {
+            Debug.Log("Нет");
+        }
+    }
+
 
     public void Fire()
     {
         Instantiate(projectile, gun.transform.position, transform.rotation);
+        
+    }
+
+
+    private void SetLaneSpawner() {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach(AttackerSpawner spawner in spawners) {
+            bool IsClosedEnough = (Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+            if (IsClosedEnough) {
+                myLaneSpawner = spawner;
+            } 
+        }
+    }
+
+    private bool IsAttackerInLane() {
+        if (myLaneSpawner.transform.childCount <= 0) {
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 }

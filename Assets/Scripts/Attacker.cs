2d30ff394c2d823748fb7402da394c602e8e422a
Alex <alexlzrw@gmail.com>
2020-10-23 18:34:38 +0300

    Attackers Striking target

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -1,16 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Attacker : MonoBehaviour
 {
    
-    float currentSpeed = 0.6f;
+    float currentSpeed = 1f;
     GameObject currentTarget;
+    Health health;
 
-    void Update()
-    {
+    void Update() {
         transform.Translate(Vector2.left * Time.deltaTime * currentSpeed);
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState() {
+        if (!currentTarget) {
+            GetComponent<Animator>().SetBool("IsAttacking", false);
+        }
     }
 
     public void SetMovementSpeed(float speed)
@@ -21,5 +29,14 @@ public class Attacker : MonoBehaviour
     public void Attack(GameObject target) {
         GetComponent<Animator>().SetBool("IsAttacking", true);
         currentTarget = target;
+    }
+
+    public void StrikeCurrentTarget(float damage) {
+        if (!currentTarget) { return; }
+
+        health = currentTarget.GetComponent<Health>();
+        if (health) {
+            health.DealDamage(damage);
+        }
     }
 }

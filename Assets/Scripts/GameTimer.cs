300fe1d74f7998f0407ee530d3456d77fb76ebac
Alex <alexlzrw@gmail.com>
2020-11-02 21:40:40 +0300

    add slider and level timer

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour {
+
+    [Tooltip("Level time in seconds")]
+    [SerializeField] float levelTime = 10f;
+
+
+    void Update() {
+        TimerFinished(); 
+    }
+
+    private void TimerFinished() {
+        GetComponent<Slider>().value = Time.timeSinceLevelLoad / levelTime;
+
+        bool timerFinished = Time.timeSinceLevelLoad >= levelTime;
+        if (timerFinished) {
+            FindObjectOfType<LoadScene>().LoadGameOver();
+        }
+    }
+}
